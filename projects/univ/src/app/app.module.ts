ca179fe0262a0e56e192a6b8f08f9f257c2c27a4
Marian Stoica <marian.stoica@web-prime.ro>
2018-07-09 02:08:31 +0300

    Added @angular/material

--- projects/univ/src/app/app.module.ts
+++ projects/univ/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
+import { DashComponent } from './dash/dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/univ/src/app/dash/dash.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointState, BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'univ-dash',
+  templateUrl: './dash.component.html',
+  styleUrls: ['./dash.component.css']
+})
+export class DashComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = [
+    { title: 'Universal', cols: 2, rows: 1 },
+    { title: 'Card 2', cols: 1, rows: 1 },
+    { title: 'Card 3', cols: 1, rows: 1 },
+    { title: 'Card 4', cols: 1, rows: 1 }
+  ];
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
